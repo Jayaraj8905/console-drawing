33e586c065cb2913ed9ce381a05c6bae6e62658b
Jayaraj <jayaraj8905@gmail.com>
2022-04-23 13:35:47 +0800

    Inquirer and debug dependencies

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-console.log("aa");
+// HIGH LEVEL TASKS
+
+// 1. PROMPTER - https://www.digitalocean.com/community/tutorials/nodejs-interactive-command-line-prompts
+// 2. VALIDATION HANDLER
+// 3. CANVAS BOARD
+// 4. RECTANGLE
+// 5. SQUARE
+// 6. FILL - FLOOD FILL ALGORITHM
+// 7. CANVAS DRAWING
+
+// https://medium.com/@hiteshkrsahu/using-es6-in-node-js-server-eba548f013b3
+// https://michele.io/nodemon-babel-vscode/
+
+import inquirer from  'inquirer';
+class Interaction {
+
+    constructor() {
+        this.prompt();
+    }
+
+    /**
+     * Prompt and validate the user input
+     */
+    prompt() {
+        inquirer.prompt([{
+                type: 'input',
+                name: 'command',
+                message: 'Enter command:'
+            }]).then(answers => {
+                console.log('\n');
+                console.log(answers);
+        });
+    }
+} 
+
+new Interaction();
+
